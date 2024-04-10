ea703922bf1767f7ca4e9eb1b1d5830286e16cc1
Artem Kolomeetc <artesdi@gmail.com>
2024-04-09 23:21:50 -0700

    iterm2 vim comment, duplicate line

--- nvim-lazyvim/lua/config/keymaps.lua
+++ nvim-lazyvim/lua/config/keymaps.lua
@@ -8,5 +8,8 @@ vim.keymap.set(
 vim.keymap.set("n", "<C-`>", "<cmd>BufferLineCycleNext<CR>", { noremap = false, desc = "Next Buffer" })
 vim.keymap.set("n", "<C-S-Right>", "<cmd>vertical resize +10<CR>", { desc = "Increase window width" })
 vim.keymap.set("n", "<C-S-Left>", "<cmd>vertical resize -10<CR>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -10<CR>", { desc = "Decrease window width" })
 vim.keymap.set("n", "<C-S-Up>", "<cmd>resize +5<CR>", { desc = "Increase window height" })
 vim.keymap.set("n", "<C-S-Down>", "<cmd>resize -5<CR>", { desc = "Decrease window height" })
+
+-- vim.keymap.set("n", "<C-d>", "<cmd>copy .<CR>", { desc = "Duplicate line" })
