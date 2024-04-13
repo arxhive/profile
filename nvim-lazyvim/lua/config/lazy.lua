69b4bbdfad558c51bcb585def151c6e83d19200b
Artem Kolomeetc <artesdi@gmail.com>
2024-04-12 23:21:33 -0700

    cleaned up lazy.lua

--- nvim-lazyvim/lua/config/lazy.lua
+++ nvim-lazyvim/lua/config/lazy.lua
@@ -9,22 +9,13 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
-    -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
-    -- By default, only LazyVim plugins wiLl be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you Can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=fAlse for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may bReak your Neovim install.
     version = false, -- always use the latEst git commit
-    -- version = "*", -- try installing thE latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
