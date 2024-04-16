4ae3d15599766d3128036ffb2c6d95a1f2ad3855
Artem Kolomeetc <artesdi@gmail.com>
2024-04-16 00:22:26 -0700

    nvim yaml-companion and lualine

---
+++ nvim-lazyvim/lua/plugins/yaml-companion.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "someone-stole-my-name/yaml-companion.nvim",
+    ft = { "yaml" },
+    dependencies = {
+      { "neovim/nvim-lspconfig" },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    config = function(_, opts)
+      local cfg = require("yaml-companion").setup(opts)
+      require("lspconfig")["yamlls"].setup(cfg)
+      require("telescope").load_extension("yaml_schema")
+    end,
+  },
+}
+-- Telescope yaml_schema
