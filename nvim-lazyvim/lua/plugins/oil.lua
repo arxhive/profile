717d246b1a8bb0a026f1128ab8dda27865467afc
Artem Kolomeetc <artesdi@gmail.com>
2024-04-15 23:51:23 -0700

    nvim oil keymaps

--- nvim-lazyvim/lua/plugins/oil.lua
+++ nvim-lazyvim/lua/plugins/oil.lua
@@ -4,6 +4,9 @@ return {
   opts = {
     skip_confirm_for_simple_edits = true,
     prompt_save_on_select_new_entry = false,
+    keymaps = {
+      ["<bs>"] = "actions.parent",
+    },
   },
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
