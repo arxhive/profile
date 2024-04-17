58b1884e28b69159120f132f69a0be7a123b362c
Artem Kolomeetc <artesdi@gmail.com>
2024-04-17 16:33:15 -0700

    adjusted neotest hotkeys

--- nvim-lazyvim/lua/plugins/neotest.lua
+++ nvim-lazyvim/lua/plugins/neotest.lua
@@ -7,25 +7,11 @@ return {
       "marilari88/neotest-vitest",
     },
     keys = {
-      {
-        "<leader>tl",
-        function()
-          require("neotest").run.run_last()
-        end,
-        desc = "Run Last Test",
-      },
-      {
-        "<leader>tL",
-        function()
-          require("neotest").run.run_last({ strategy = "dap" })
-        end,
-        desc = "Debug Last Test",
-      },
-      {
-        "<leader>tw",
-        "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
-        desc = "Run Watch",
-      },
+      { "<leader>tT", function() require("neotest").run.run({ suite = true }) end, desc = "Run All Test Files", },
+      { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last Test", },
+      { "<leader>tL", function() require("neotest").run.run_last({ strategy = "dap" }) end, desc = "Debug Last Test", },
+      { "<leader>tw", "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>", desc = "Run Watch", },
+      { "<leader>tu", function() require("neotest").summary.toggle() end, desc = "Toggle Summary UI" },
     },
     opts = function(_, opts)
       table.insert(
