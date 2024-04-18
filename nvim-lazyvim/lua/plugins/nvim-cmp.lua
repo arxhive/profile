2f860d2386162b13befcd501d9a352cce011175d
Artem Kolomeetc <artesdi@gmail.com>
2024-04-17 22:49:05 -0700

    nvim noise + cmp-cmdline

--- nvim-lazyvim/lua/plugins/nvim-cmp.lua
+++ nvim-lazyvim/lua/plugins/nvim-cmp.lua
@@ -1,13 +1,12 @@
 return {
-  -- Use <tab> for completion and snippets (supertab)
-  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
+  { "hrsh7th/cmp-cmdline" },
   {
+  -- disable default <tab> and <s-tab> behavior in LuaSnip
     "L3MON4D3/LuaSnip",
     keys = function()
       return {}
     end,
   },
-  -- then: setup supertab in cmp
   {
     "hrsh7th/nvim-cmp",
     ---@param opts cmp.ConfigSchema
@@ -21,10 +20,11 @@ return {
       local luasnip = require("luasnip")
       local cmp = require("cmp")
 
+    -- setup tab complete for cmp and luasnip
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace })
+            cmp.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace })
           -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
           -- this way you will only jump inside the snippet region
           elseif luasnip.expand_or_jumpable() then
@@ -45,6 +45,27 @@ return {
           end
         end, { "i", "s" }),
       })
+      -- `:` cmdline setup.
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+            {
+              name = "cmdline",
+              option = {
+                ignore_cmds = { "Man", "!" },
+              },
+            },
+          }),
+      })
+     -- `:/` cmdline setup.
+     cmp.setup.cmdline("/", {
+       mapping = cmp.mapping.preset.cmdline(),
+       sources = {
+         { name = "buffer" },
+       },
+     })
     end,
   },
 }
