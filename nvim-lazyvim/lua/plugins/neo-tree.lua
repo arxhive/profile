a1f07604f7af2f57adb59d25db1e90e881e0fbcd
Artem Kolomeetc <artesdi@gmail.com>
2024-04-20 19:07:01 -0700

    rewrote nvim neo-tree config

--- nvim-lazyvim/lua/plugins/neo-tree.lua
+++ nvim-lazyvim/lua/plugins/neo-tree.lua
@@ -4,23 +4,45 @@ return {
   dependencies = {
     "miversen33/netman.nvim",
   },
-  sources = {
-    "filesystem",
-    "netman.ui.neo-tree",
-  },
-  config = function()
-    require("neo-tree").setup({
-      sources = {
-        "filesystem",
-        "buffers",
-        "git_status",
-        "document_symbols",
-        "netman.ui.neo-tree",
-      },
-    })
+  init = function()
+    if vim.fn.argc(-1) == 1 then
+      local stat = vim.uv.fs_stat(vim.fn.argv(0))
+      if stat and stat.type == "directory" then
+        require("neo-tree")
+      end
+    end
   end,
   opts = {
+    config = function(_, opts)
+      local function on_move(data)
+        LazyVim.lsp.on_rename(data.source, data.destination)
+      end
+
+      local events = require("neo-tree.events")
+      opts.event_handlers = opts.event_handlers or {}
+      vim.list_extend(opts.event_handlers, {
+        { event = events.FILE_MOVED, handler = on_move },
+        { event = events.FILE_RENAMED, handler = on_move },
+      })
+      require("neo-tree").setup(opts)
+      vim.api.nvim_create_autocmd("TermClose", {
+        pattern = "*lazygit",
+        callback = function()
+          if package.loaded["neo-tree.sources.git_status"] then
+            require("neo-tree.sources.git_status").refresh()
+          end
+        end,
+      })
+    end,
+    sources = { "filesystem", "buffers", "git_status", "document_symbols", "netman.ui.neo-tree" },
+    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
     filesystem = {
+      bind_to_cwd = false,
+      use_libuv_file_watcher = true,
+      follow_current_file = {
+        enabled = true,
+        show_root = true,
+      },
       filtered_items = {
         visible = true,
         hide_hidden = false,
@@ -43,11 +65,38 @@ return {
         },
       },
     },
-    follow_current_file = {
-      enable = true,
-      show_root = true,
+    window = {
+      mappings = {
+        ["<space>"] = "none",
+        ["Y"] = {
+          function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            vim.fn.setreg("+", path, "c")
+          end,
+          desc = "Copy Path to Clipboard",
+        },
+        ["O"] = {
+          function(state)
+            require("lazy.util").open(state.tree:get_node().path, { system = true })
+          end,
+          desc = "Open with System Application",
+        },
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
     },
   },
+  cmd = "Neotree",
+  deactivate = function()
+    vim.cmd([[Neotree close]])
+  end,
   keys = {
     {
       "<C-S-Q>",
