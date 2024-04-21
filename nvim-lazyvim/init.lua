359eed9e355d6cd0f59d61fd72c1f20f48ae9742
Artem Kolomeetc <artesdi@gmail.com>
2024-04-20 23:21:27 -0700

    moved nvim settings to options

--- nvim-lazyvim/init.lua
+++ nvim-lazyvim/init.lua
@@ -1,6 +1,3 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("oil").setup({})
--- nvim-lazyvim/lua/config/options.lua
+++ nvim-lazyvim/lua/config/options.lua
@@ -5,6 +5,14 @@ vim.opt.swapfile = false
 vim.opt.scrolloff = 10
 vim.opt.wrap = false
 
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.showtabline = 2 -- show always, to avoid conflicts with lualine config
+
+vim.opt.list = false -- hide ident marks in the insert mode
+
 require("lazyvim.util").lsp.on_attach(function()
   vim.opt.signcolumn = "yes"
 end)
