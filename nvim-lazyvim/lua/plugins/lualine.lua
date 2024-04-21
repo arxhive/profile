3a14c8180adaea942af45115c899156d9a66ab68
Artem Kolomeetc <artesdi@gmail.com>
2024-04-20 17:30:09 -0700

    nvim lazy yaml-companion

--- nvim-lazyvim/lua/plugins/lualine.lua
+++ nvim-lazyvim/lua/plugins/lualine.lua
@@ -9,16 +9,16 @@ return {
       lualine_c = { { "filetype", icon_only = true }, { "filename", file_status = true, path = 1 } },
       lualine_x = {},
       lualine_y = {
-        -- display yaml/Kubernetes
-        {
-          function()
-            local schema = require("yaml-companion").get_buf_schema(0)
-            if schema.result[1].name == "none" then
-              return ""
-            end
-            return schema.result[1].name
-          end,
-        },
+        -- display yaml/Kubernetes (~15ms)
+        -- {
+        --   function()
+        --     local schema = require("yaml-companion").get_buf_schema(0)
+        --     if schema.result[1].name == "none" then
+        --       return ""
+        --     end
+        --     return schema.result[1].name
+        --   end,
+        -- },
         -- or rest client env (disabled because the rest client is very expensive to load: 50ms)
         -- {
         --   "rest",
