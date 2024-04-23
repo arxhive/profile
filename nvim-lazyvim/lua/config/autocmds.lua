54a8b9c92125ee416ac715ffc48adb9fe63b12fc
Artem Kolomeetc <artesdi@gmail.com>
2024-04-23 09:49:36 -0700

    nvim telescope close on ESC

--- nvim-lazyvim/lua/config/autocmds.lua
+++ nvim-lazyvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+
+-- Leave telescope promt on ESC from insert mode
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+  callback = function(event)
+    if vim.bo[event.buf].filetype == "TelescopePrompt" then
+      require("telescope.actions").close(vim.api.nvim_get_current_buf())
+      -- vim.api.nvim_input("<ESC>")
+    end
+  end,
+})
