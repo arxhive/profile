202ebc47b3d7bcd8cca530d91ed2ab47b286114a
Artem Kolomeetc <artesdi@gmail.com>
2024-04-27 18:52:22 -0700

    nvim telescope remap grep and string search

--- nvim-lazyvim/lua/plugins/telescope.lua
+++ nvim-lazyvim/lua/plugins/telescope.lua
@@ -13,8 +13,10 @@ return {
     },
     -- stylua: ignore
     keys = {
-      { "<C-S-P>", function() require("telescope").extensions.yank_history.yank_history() end, mode = { "n", "x" }, { desc = "Telescope yank history" } },
+      { "<C-S-p>", function() require("telescope").extensions.yank_history.yank_history() end, mode = { "n", "x" }, { desc = "Telescope yank history" } },
       { "<leader>snt", function() vim.cmd("Noice telescope") end, { desc = "Noice telescope" } },
+      { "<C-f>", function() vim.cmd("Telescope live_grep") end, { desc = "Grep" } },
+      { "<C-S-f>", function() vim.cmd("Telescope grep_string") end, { desc = "Grep string" } },
     },
   },
   {
@@ -23,7 +25,7 @@ return {
     module = "telescope._extensions.luasnip", -- used for lazy loading
     keys = {
       -- stylua: ignore
-      { "<leader>sl", function() require("telescope").extensions.luasnip.luasnip() end, { noremap = true, silent = true, desc = "luasnip" } },
+      { "<leader>sl", function() require("telescope").extensions.luasnip.luasnip() end, { noremap = true, silent = true, desc = "luasnip" } }, -- fix whichkey
     },
   },
 }
