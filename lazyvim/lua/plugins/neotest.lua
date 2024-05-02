90b5361aff0b0cc4442d6a96151d79a8dc077dfc
Artem Kolomeetc <artesdi@gmail.com>
2024-05-01 21:53:40 -0700

    neotest summary automatic

--- lazyvim/lua/plugins/neotest.lua
+++ lazyvim/lua/plugins/neotest.lua
@@ -6,10 +6,33 @@ return {
       "haydenmeade/neotest-jest",
       "marilari88/neotest-vitest",
     },
+    -- stylua: ignore
     keys = {
-      { "<leader>tT", function() require("neotest").run.run({ suite = true }) end, desc = "Run All Test Files", },
-      { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last Test", },
-      { "<leader>tL", function() require("neotest").run.run_last({ strategy = "dap" }) end, desc = "Debug Last Test", },
+      { "<leader>tr", function() 
+              require("neotest").run.run()
+              require("neotest").summary.open()
+            end, desc = "Run Nearest", },
+
+      { "<leader>tt", function() 
+        require("neotest").run.run(vim.fn.expand("%"))
+        require("neotest").summary.open()
+      end, desc = "Run File", },
+
+      { "<leader>tT", function() 
+        require("neotest").run.run({ suite = true }) 
+        require("neotest").summary.open()
+      end, desc = "Run All Test Files", },
+
+      { "<leader>tl", function() 
+        require("neotest").run.run_last() 
+        require("neotest").summary.open()
+      end, desc = "Run Last Test", },
+
+      { "<leader>tL", function() 
+        require("neotest").run.run_last({ strategy = "dap" }) 
+        require("neotest").summary.open()
+      end, desc = "Debug Last Test", },
+
       { "<leader>tw", "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>", desc = "Run Watch", },
       { "<leader>tu", function() require("neotest").summary.toggle() end, desc = "Toggle Summary UI" },
       { "<C-S-T>", function() require("neotest").summary.toggle() end, desc = "Toggle Summary UI" },
