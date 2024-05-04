43f404058c49e4c14758da3d761281ae72bb61f3
Artem Kolomeetc <artesdi@gmail.com>
2024-05-04 13:25:14 -0700

    nvim simplified ray-x/go

--- lazyvim/lua/plugins/go/go.lua
+++ lazyvim/lua/plugins/go/go.lua
@@ -1,13 +1,11 @@
 return {
   "ray-x/go.nvim",
-  lazy = true,
+  ft = { "go", "gomod" },
   dependencies = { -- optional packages
     "ray-x/guihua.lua",
   },
   config = function()
     require("go").setup()
   end,
-  event = { "CmdlineEnter" },
-  ft = { "go", "gomod" },
   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 }
