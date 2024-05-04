d9dd8f39fcea4d60f6c66aaa259166c593a7e219
Artem Kolomeetc <artesdi@gmail.com>
2024-05-04 10:29:39 -0700

    nvim-cmp dependencies

--- lazyvim/lua/plugins/nvim-cmp.lua
+++ lazyvim/lua/plugins/nvim-cmp.lua
@@ -9,10 +9,13 @@ return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
       "chrisgrieser/cmp_yanky",
+      "L3MON4D3/LuaSnip",
     },
-    lazy = true,
     event = { "InsertEnter", "CmdlineEnter" },
 
     ---@param opts cmp.ConfigSchema
