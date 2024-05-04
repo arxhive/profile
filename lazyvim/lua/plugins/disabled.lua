ad54b977d47013a94d8143f62314a22b1b029adc
Artem Kolomeetc <artesdi@gmail.com>
2024-05-04 12:24:18 -0700

    disabled telescope terraforms

--- lazyvim/lua/plugins/disabled.lua
+++ lazyvim/lua/plugins/disabled.lua
@@ -2,5 +2,7 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-treesitter/nvim-treesitter-context", enabled = false },
   { "karb94/neoscroll.nvim", enabled = false },
-  -- { "nosduco/remote-sshfs.nvim", enabled = false },
+  { "ANGkeith/telescope-terraform-doc.nvim", enabled = false },
+  { "cappyzawa/telescope-terraform.nvim", enabled = false },
+  { "nosduco/remote-sshfs.nvim", enabled = false },
 }
