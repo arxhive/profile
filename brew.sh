8442a0d23baf9316769b9cb7ee26cdd6a4f713d8
Artem Kolomeetc <artesdi@gmail.com>
2024-05-04 18:48:07 -0700

    switched to powerlevel10k

--- brew.sh
+++ brew.sh
@@ -12,8 +12,8 @@ brew install --cask iterm2
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
-git clone https://github.com/romkatv/zsh-defer.git $ZSH_CUSTOM/plugins/zsh-defer
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+# git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
 
 #devenv
 brew install tfenv
