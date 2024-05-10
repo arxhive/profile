543a294c6ea7a509709a54277f7c1894dde7e824
Artem Kolomeetc <artesdi@gmail.com>
2024-05-10 09:20:09 -0700

    nvim nordic theme

--- lazyvim/lua/plugins/colorscheme.lua
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -14,10 +14,18 @@ return {
     opts = { style = "storm" },
   },
   {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+    -- config = function()
+    --   require("nordic").load()
+    -- end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "tokyonight",
-      colorscheme = "catppuccin",
+      colorscheme = "nordic",
     },
   },
 }
