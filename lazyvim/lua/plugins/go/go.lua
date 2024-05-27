9c3659c96ef9459577e66a20e9d813149921cd7a
Artem Kolomeetc <artesdi@gmail.com>
2024-05-27 15:12:18 -0700

    updated go test keys

--- lazyvim/lua/plugins/go/go.lua
+++ lazyvim/lua/plugins/go/go.lua
@@ -12,10 +12,12 @@ return {
   keys = {
     -- stylua: ignore start
     { "<leader>tgs", function () vim.cmd("GoTestFunc -s") end, desc = "Test Select Function" },
-    { "<leader>tgr", function () vim.cmd("GoTestFunc -F") end, desc = "Test Function" },
+    { "<leader>tgr", function () vim.cmd("GoTestFunc -v -F") end, desc = "Test Function" },
     { "<leader>tgt", function () vim.cmd("GoTestFile -v -F") end, desc = "Test File" },
     { "<leader>tgT", function () vim.cmd("GoTest -F") end, desc = "Test All" },
     { "<leader>tgp", function () vim.cmd("GoTestPkg -F") end, desc = "Test Package" },
+    { "<leader>tgd", function () vim.cmd("GoDebug --test") end, desc = "Debug Test" },
+    { "<leader>tgg", function () vim.cmd("GinkgoFunc") end, desc = "Test Ginkgo Func" },
 
     { "<leader>cgt", function () vim.cmd("GoModTidy") end, desc = "Go Mod Tidy" },
     { "<leader>cgd", function () vim.cmd("GoDebug") end, desc = "Go Debug" },
