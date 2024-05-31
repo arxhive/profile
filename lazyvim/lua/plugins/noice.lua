e03f708a272ed39660e1a96552f948dfad65c433
Artem Kolomeetc <artesdi@gmail.com>
2024-05-31 15:16:08 -0700

    one more messages hotkey

--- lazyvim/lua/plugins/noice.lua
+++ lazyvim/lua/plugins/noice.lua
@@ -24,6 +24,7 @@ return {
             { "<leader>nl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
             { "<leader>nh", function() require("noice").cmd("history") end, desc = "Noice History" },
             { "<leader>m", function() require("noice").cmd("all") end, desc = "Messages" },
+            { "<C-S-m>", function() require("noice").cmd("all") end, desc = "Messages" },
             { "<leader>na", function() require("noice").cmd("all") end, desc = "Noice All" },
             { "<leader>nd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
             { "<leader>nt", function() vim.cmd("Noice telescope") end, desc = "Noice telescope" },
