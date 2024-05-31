3c27bca4d7a581072472656bec416fa5c3755883
Artem Kolomeetc <artesdi@gmail.com>
2024-05-31 14:16:00 -0700

    Swapped ref and harpoon keys

--- lazyvim/lua/plugins/harpoon.lua
+++ lazyvim/lua/plugins/harpoon.lua
@@ -8,8 +8,8 @@ return {
         LazyVim.info("Harpooned " .. vim.fn.expand("%:h:p"), { title = "Harpoon" })
       end, desc = "Harpoon File" },
       { "<C-S-E>", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Harpoon Menu"},
-      { "<PageUp>", function() require("harpoon"):list():prev() end, desc = "Prev Harpoon"},
-      { "<PageDown>", function() require("harpoon"):list():next() end, desc = "Next Harpoon"}
+      { "@", function() require("harpoon"):list():prev() end, desc = "Prev Harpoon"},
+      { "!", function() require("harpoon"):list():next() end, desc = "Next Harpoon"}
     },
   },
 }
