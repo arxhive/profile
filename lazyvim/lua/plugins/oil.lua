80c76eacca422117fc98c0993e8eecd3909f1d66
Artem Kolomeetc <artesdi@gmail.com>
2024-06-05 09:54:34 -0700

    oil root mapping

--- lazyvim/lua/plugins/oil.lua
+++ lazyvim/lua/plugins/oil.lua
@@ -5,7 +5,9 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   -- stylua: ignore
   keys = {
-    { "<leader>ww", function() require("oil").open() end, desc = "Oil open" },
-    { "<leader>o", function() require("oil").open() end, desc = "Oil open" },
+    { "<leader>ww", function() require("oil").open() end, desc = "Oil (cwd)" },
+    { "<leader>wW", function() require("oil").open(LazyVim.root()) end, desc = "Oil (root)" },
+    { "<leader>o", function() require("oil").open() end, desc = "Oil (cwd)" },
+    { "<leader>O", function() require("oil").open(LazyVim.root()) end, desc = "Oil (root)" },
   },
 }
