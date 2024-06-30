c6a0c6a42357d80340b0493f228b4e30084794c2
Artem Kolomeetc <artesdi@gmail.com>
2024-06-29 19:57:04 -0700

    null_ls no gopls

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -60,7 +60,7 @@ vim.api.nvim_create_user_command("Kindle", function()
     want("copilot")
 
     -- Remap LSP keys according to the documentation
-    -- https://www.lazyvim.org/plugins/lspi
+    -- https://www.lazyvim.org/plugins/lsp
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     keys[#keys + 1] = { "<leader>cc", false }
     keys[#keys + 1] = { "<leader>cC", false }
--- lazyvim/lua/plugins/lsp/none-ls.lua
+++ lazyvim/lua/plugins/lsp/none-ls.lua
@@ -1,16 +1,15 @@
 return {
   {
     "nvimtools/none-ls.nvim",
+    enabled = false,
     lazy = true,
     event = function(_, event)
       return {}
     end,
     opts = function(_, opts)
       local null_ls = require("null-ls")
-      null_ls.disable({
-        name = "spell",
-        method = "completion",
-      })
+      null_ls.disable({ name = "spell", method = "completion" })
+      null_ls.disable({ name = "gopls" })
     end,
   },
 }
