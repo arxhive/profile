da4800a5f79c5aaa1f7570171aedd3d6bf7ffa6e
Artem Kolomeetc <artesdi@gmail.com>
2024-06-30 22:16:23 -0700

    Trouble jump and close

--- lazyvim/lua/plugins/trouble.lua
+++ lazyvim/lua/plugins/trouble.lua
@@ -1,8 +1,13 @@
 return {
   "folke/trouble.nvim",
   opts = {
+    ---@type trouble.Window.opts
     win = {
       type = "float",
+    },
+    ---@type table<string, trouble.Action.spec>
+    keys = {
+      ["<cr>"] = "jump_close",
     },
   },
 }
