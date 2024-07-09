72cafa45c9fadac4128af2674913bb4d9efb9f63
Artem Kolomeetc <artesdi@gmail.com>
2024-07-09 00:15:54 -0700

    small fix toggleterm close

--- lazyvim/lua/plugins/toggleterm.lua
+++ lazyvim/lua/plugins/toggleterm.lua
@@ -12,8 +12,9 @@ return {
     {
       "<leader>bc",
       function()
-        -- i is either insert mode or alias iexit=exit 
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-l>i", true, false, true), "m", false)
+        -- 3 <C-l> just in case if I have more than 1 vsplit open
+        -- i is either insert mode or alias iexit=exit
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-l><C-l><C-l>i", true, false, true), "m", false)
         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-c>", true, false, true), "m", false)
         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("exit<CR>", true, false, true), "m", false)
       end,
