3ccf2211bac7c1d24a41e292c8a4c7d4eabe075d
Artem Kolomeetc <artesdi@gmail.com>
2025-03-20 19:04:45 -0700

    copilot promts

--- lazyvim/lua/plugins/copilot-chat.lua
+++ lazyvim/lua/plugins/copilot-chat.lua
@@ -1,3 +1,21 @@
+local prompts = {
+  -- Code related prompts
+  Explain = "Please explain how the following code works.",
+  Review = "Please review the following code and provide suggestions for improvement.",
+  Tests = "Please explain how the selected code works, then generate unit tests for it.",
+  Refactor = "Please refactor the following code to improve its clarity and readability.",
+  FixCode = "Please fix the following code to make it work as intended.",
+  FixError = "Please explain the error in the following text and provide a solution.",
+  BetterNamings = "Please provide better names for the following variables and functions.",
+  Documentation = "Please provide documentation for the following code.",
+  SwaggerApiDocs = "Please provide documentation for the following API using Swagger.",
+  -- Text related prompts
+  Summarize = "Please summarize the following text.",
+  Spelling = "Please correct any grammar and spelling errors in the following text.",
+  Wording = "Please improve the grammar and wording of the following text.",
+  Concise = "Please rewrite the following text to make it more concise.",
+}
+
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
@@ -10,6 +28,7 @@ return {
         insert_at_end = true,
         clear_chat_on_new_prompt = false,
         highlight_selection = false,
+        prompts = prompts,
         mappings = {
           complete = {
             insert = "<C-CR>",
