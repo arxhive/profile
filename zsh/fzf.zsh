8b506d88b763b51d51e60b5a36d044519b2eca19
Artem Kolomeetc <artesdi@gmail.com>
2025-03-29 16:50:12 -0700

    fzf-files tail and helpers

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -8,8 +8,9 @@ export FZF_DEFAULT_OPTS="
   --bind 'ctrl-l:become(less +G {})' \
   --bind 'ctrl-p:become(bat {})' \
   --bind 'ctrl-j:become(cat {} | jq)' \
+  --bind 'ctrl-t:become(tail -f {})' \
   --bind 'ctrl-o:become(open {})' \
-  --bind 'ctrl-t:become(realpath {})' \
+  --bind 'ctrl-h:become(realpath {})' \
   --bind 'ctrl-b:become(/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome {})' \
   --bind 'ctrl-s:become(/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl {})'
 "
@@ -24,6 +25,7 @@ export FZF_DEFAULT_COMMAND="
 export FZF_CTRL_T_COMMAND="fd --type f"
 
 alias files-fzf="fzf \
+    --header $'CTRL-D Dirs, CRTL-K Keys' \
     --preview 'bat --color=always {}' \
     --preview-window border-none,follow \
     --bind 'enter:become(echo __nvim__:{})' \
@@ -31,18 +33,20 @@ alias files-fzf="fzf \
     --bind 'ctrl-l:become(echo __less__:{})' \
     --bind 'ctrl-p:become(echo __bat__:{})' \
     --bind 'ctrl-j:become(echo __cat__:{})' \
+    --bind 'ctrl-t:become(echo __tail__:{})' \
     --bind 'ctrl-o:become(echo __finder__:{})' \
     --bind 'ctrl-b:become(echo __chrome__:{})' \
     --bind 'ctrl-s:become(echo __sublime__:{})' \
-    --bind 'ctrl-t:become(echo __path__:{})' \
-    --bind 'ctrl-f:become(echo __dirs__)'"
+    --bind 'ctrl-h:become(echo __path__:{})' \
+    --bind 'ctrl-k:become(echo __help__:{})' \
+    --bind 'ctrl-d:become(echo __dirs__)'"
 
 ff() {
   selected="$(files-fzf)"
 
   if [[ $? -eq 0 ]]; then
     if [ "$selected" = "__dirs__" ]; then
-      cf
+      cff
     elif [[ "$selected" == __nvim__:* ]]; then
       nvim "${selected#__nvim__:}"
     elif [[ "$selected" == __less__:* ]]; then
@@ -51,6 +55,8 @@ ff() {
       bat "${selected#__bat__:}"
     elif [[ "$selected" == __cat__:* ]]; then
       cat "${selected#__cat__:}" | jq
+    elif [[ "$selected" == __tail__:* ]]; then
+      tail -f "${selected#__tail__:}"
     elif [[ "$selected" == __finder__:* ]]; then
       open "${selected#__finder__:}"
     elif [[ "$selected" == __chrome__:* ]]; then
@@ -59,6 +65,18 @@ ff() {
       /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl "${selected#__sublime__:}"
     elif [[ "$selected" == __path__:* ]]; then
       realpath "${selected#__path__:}"
+    elif [[ "$selected" == __help__:* ]]; then
+      echo "CTRL-D: Dirs"
+      echo "CTRL-K: Keys"
+      echo "CTRL-V: Open in nvim"
+      echo "CTRL-L: Open in less"
+      echo "CTRL-P: Print in bat"
+      echo "CTRL-J: Cat json"
+      echo "CTRL-T: Tail logs"
+      echo "CTRL-O: Open in finder"
+      echo "CTRL-B: Open in Chrome"
+      echo "CTRL-S: Open in Sublime Text"
+      echo "CTRL-H: Print realpath"
     fi
   fi
 }
@@ -77,6 +95,8 @@ f() {
       bat "${selected#__bat__:}"
     elif [[ "$selected" == __cat__:* ]]; then
       cat "${selected#__cat__:}" | jq
+    elif [[ "$selected" == __tail__:* ]]; then
+      tail -f "${selected#__tail__:}"
     elif [[ "$selected" == __finder__:* ]]; then
       open "${selected#__finder__:}"
     elif [[ "$selected" == __chrome__:* ]]; then
@@ -85,22 +105,39 @@ f() {
       /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl "${selected#__sublime__:}"
     elif [[ "$selected" == __path__:* ]]; then
       realpath "${selected#__path__:}"
+    elif [[ "$selected" == __help__:* ]]; then
+      echo "CTRL-D: Dirs"
+      echo "CTRL-K: Keys"
+      echo "CTRL-V: Open in nvim"
+      echo "CTRL-L: Open in less"
+      echo "CTRL-P: Print in bat"
+      echo "CTRL-J: Cat json"
+      echo "CTRL-T: Tail logs"
+      echo "CTRL-O: Open in finder"
+      echo "CTRL-B: Open in Chrome"
+      echo "CTRL-S: Open in Sublime Text"
+      echo "CTRL-H: Print realpath"
     fi
   fi
 }
 
-alias cff='cd ./"$(fd -H --type d | fzf)"'
+cff() {
+  cd ./"$(fd -H --type d | fzf)"
+}
+
 cf() {
   selected="$(
     (
       echo '..'
       fd -H --type d --max-depth 1
     ) | fzf \
+      --header $'CTRL-F Files, CRTL-K Keys' \
       --bind 'ctrl-r:become(echo ..)' \
       --preview 'pwd' \
       --preview-window 1,top,border-none \
       --bind 'ctrl-f:become(echo __files__)' \
       --bind 'ctrl-v:become(echo __nvim__)' \
+      --bind 'ctrl-k:become(echo __help__:{})' \
       --bind 'ctrl-o:become(echo __open__)'
   )"
 
@@ -112,6 +149,11 @@ cf() {
       nvim .
     elif [ "$selected" = "__open__" ]; then
       open .
+    elif [[ "$selected" == __help__:* ]]; then
+      echo "CTRL-D: Dirs"
+      echo "CTRL-K: Keys"
+      echo "CTRL-V: Open current dir in nvim"
+      echo "CTRL-O: Open in finder"
     else
       cd $selected
       cf
